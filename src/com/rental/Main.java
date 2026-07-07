b255e6e85b25c62d271971c2eee45ae37cf87b8b
Ahmed Naveed <ahmednaveed2526@gmail.com>
2026-07-07 22:35:53 +0530

    Update Main.java

--- src/com/rental/Main.java
+++ src/com/rental/Main.java
@@ -4,7 +4,7 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // This tells Java to safely start the GUI on the correct thread
+      
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
