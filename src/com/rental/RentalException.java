e062c73392712eaaf3f59302629129cc8f7250d0
Ahmed Naveed <ahmednaveed2526@gmail.com>
2026-07-07 22:36:10 +0530

    Update RentalException.java

--- src/com/rental/RentalException.java
+++ src/com/rental/RentalException.java
@@ -1,6 +1,6 @@
 package com.rental;
 
-// This makes our class act like a professional error message
+
 public class RentalException extends Exception {
     public RentalException(String message) {
         super(message);
